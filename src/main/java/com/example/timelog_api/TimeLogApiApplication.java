253ad9f5aa13e6d0bfcd6326a30eb7c87368e902
Bryła <csowscy@gmail.com>
2024-10-12 16:31:20 +0200

    Add connection to DB

--- src/main/java/com/example/timelog_api/TimeLogApiApplication.java
+++ src/main/java/com/example/timelog_api/TimeLogApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.timelog_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimeLogApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv d = Dotenv.load();
+		String dbUsername = d.get("DB_USERNAME");
+		String dbPassword = d.get("DB_PASSWORD");
+		String dbUrl = d.get("DB_URL");
+		if (dbUsername == null || dbPassword == null || dbUrl == null) {
+			System.err.println("One or more required environment variables are missing. Please check your .env file.");
+			System.exit(1);
+		}
+		System.setProperty("DB_USERNAME", dbUsername);
+		System.setProperty("DB_PASSWORD", dbPassword);
+		System.setProperty("DB_URL", dbUrl);
+
 		SpringApplication.run(TimeLogApiApplication.class, args);
 	}
 
