aeff764b8b2122e28e94c1dddec9486637f319af
Bryła <csowscy@gmail.com>
2024-10-12 15:37:32 +0200

    init commit

---
+++ src/test/java/com/example/timelog_api/TimeLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timelog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
